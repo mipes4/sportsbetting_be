1f280975fa34864ecfa071488f96638236c8d8e0
mipes4 <michellepesch@gmail.com>
2020-06-30 11:08:55 +0200

    Set up the index.js, made a server and is listening

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const corsMiddleWare = require("cors");
+const { PORT } = require("./config/constants");
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
