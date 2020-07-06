844b67ca2f537a1380056075d99d2b4be55d8bba
mipes4 <michellepesch@gmail.com>
2020-07-06 21:51:54 +0200

    Added configs, made JWT key, added Middleware and made the auth routers: POST create log in, POST create user, GET users profile with token

--- config/constants.js
+++ config/constants.js
@@ -4,4 +4,5 @@ module.exports = {
   apiUrl: process.env.API_URL,
   apiUrlDemo: process.env.API_URL_DEMO,
   apiKey: process.env.API_KEY,
+  jwtSecret: process.env.JWT_SECRET || "e9rp^&^*&@9sejg)DSUA)jpfds8394jdsfn,m",
 };
